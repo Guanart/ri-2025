0cf2dcc157eaf7e018563ebc9fbf17a4ed48e701
Gonzalo <gu4n4rt@gmail.com>
2025-03-31 02:15:59 +0000

    Ejercicio 1

--- TP01 - Analisis de Texto/ejercicio1/Tokenizador.py
+++ TP01 - Analisis de Texto/ejercicio1/Tokenizador.py
@@ -3,14 +3,22 @@
 
 import os
 import re
+import json
 
+'''
+El Tokenizador debe retornar:
+- Lista de términos y su DF
+- Cantidad de tokens
+- Cantidad de términos 
+- Cantidad de documentos procesados  
+'''
 class Tokenizador:
     def __init__(self):
         pass
 
     def tokenizar(self, texto):
         # Eliminar caracteres especiales (deja caracteres alfanumericos y espacios)
-        texto = re.sub(r'[^\w\s]', '', texto)   # \w incluye los guiones bajos "_"?
+        texto = re.sub(r'[^\w\s]|_', '', texto)   # \w incluye los guiones bajos "_"?  SI
         # Split por espacios
         tokens = texto.split()
         # Eliminar tokens vacios (cadenas vacias)
@@ -22,24 +30,34 @@ class Tokenizador:
     def analizar_coleccion(self, path_documentos) -> dict:
         docs_analizados = 0
         cantidad_tokens = 0
-        terminos = {}   # Almacena los terminos en el formato collection_data.json
-
-        for doc_id, doc_name in enumerate(os.listdir(path_documentos), start=1):    # doc0.txt tendra id=1
+        terminos = {}   # Almacena los terminos como keys, y como values los docid, freqs y DF
+        
+        for doc_id, doc_name in enumerate(os.listdir(path_documentos), start=0):
             path_doc = os.path.join(path_documentos, doc_name)
             with open(path_doc, 'r') as f:
                 texto = f.read()  # Podemos leer los archivos completos, ya que no son muy grandes y solo tienen una linea
                 tokens = self.tokenizar(texto)
                 cantidad_tokens += len(tokens)
 
-                # Por cada token, 
+                # Ordenar tokens para aplicar corte de control
+                tokens.sort()
+                i = 0
+                while i < len(tokens):
+                    current = tokens[i]
+                    count = 1
+                    j = i + 1
+                    while j < len(tokens) and tokens[j] == current:
+                        count += 1
+                        j += 1
+                    if current not in terminos:
+                        terminos[current] = {"docid": [], "freq": [], "df": 0}
+                    terminos[current]["docid"].append(doc_id)
+                    terminos[current]["freq"].append(count)
+                    terminos[current]["df"] += 1
+                    i = j
+
                 docs_analizados += 1
 
-        # {"data": [
-        #       {"term": "reloj", "docid": [2, 3, 5,... 9543], "freq": [23, 42, 12,... 20], "df": 97},
-        #       {"term": "casa", "docid": [2, 3, 5,... 9543], "freq": [23, 42, 12,... 20], "df": 97},
-        #       {"term": "perro", "docid": [2, 3, 5,... 9543], "freq": [23, 42, 12,... 20], "df": 97},
-        # ]}
-        # Asi esta formato collection_data.json. Por ahora, solo guardo el termino y su df
         return {
             "data": terminos,
             "statistics": {
@@ -51,4 +69,5 @@ class Tokenizador:
 
 tokenizador = Tokenizador()
 resultados = tokenizador.analizar_coleccion('./datos/collection_test/TestCollection/')
-print(resultados)
+with open('./output_punto1.json', 'w') as f:
+    json.dump(resultados, f, indent=2)
